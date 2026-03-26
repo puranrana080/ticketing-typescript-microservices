228f088035ff2c4fb36eb54dbd0444ee811e6599
puranrana080 <puransinghrana080@gmail.com>
2026-03-26 13:01:38 +0530

    add cookieSession

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
+
+
 import {currentUserRouter} from "./routes/current-user.ts"
 import {signinRouter} from "./routes/signin.ts"
 import {signoutRouter} from "./routes/signout.ts"
@@ -7,7 +10,9 @@ import {signupRouter} from "./routes/signup.ts"
 import { errorHandler } from './middlewares/error-handler.ts';
 import { NotFoundError } from './errors/not-found-error.ts';
 const app = express()
+app.set('trust proxy',true)
 app.use(express.json())
+app.use(cookieSession({signed:false,secure:true}))
 
 app.use(currentUserRouter)
 app.use(signinRouter)
